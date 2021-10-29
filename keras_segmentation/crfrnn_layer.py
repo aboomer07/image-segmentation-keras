ca081e667610bb143155a4d8d473ef89cc72536d
Andy Boomer <aboomer07@gmail.com>
2021-10-29 12:42:32 -0700

    Fixing TF Version

--- keras_segmentation/crfrnn_layer.py
+++ keras_segmentation/crfrnn_layer.py
@@ -24,7 +24,8 @@ SOFTWARE.
 
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.engine.topology import Layer
+# from tensorflow.keras.engine.topology import Layer
+from tensorflow.keras.layers import Layer
 
 import high_dim_filter_loader
 
