531ec5da6bf9b85b5e165764d3b82868dcee5e1b
Andy Boomer <36014921+aboomer07@users.noreply.github.com>
2021-10-31 11:15:38 -0700

    Update model_utils.py

--- keras_segmentation/models/model_utils.py
+++ keras_segmentation/models/model_utils.py
@@ -94,7 +94,7 @@ def get_segmentation_model(input, output, add_crf=False):
     o = (Activation('softmax'))(o)
     if add_crf:
         o = CRF(n_classes)(o)
-        o = Lambda(tf.cast(x, tf.float64), name='change_to_float')(o)
+        o = Lambda(lambda x: tf.cast(x, tf.float64), name='change_to_float')(o)
     model = Model(img_input, o)
     model.output_width = output_width
     model.output_height = output_height
